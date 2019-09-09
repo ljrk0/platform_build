3ed7966cda97a96473994400e58409f6aa631688
Daniel Micay <danielmicay@gmail.com>
2019-09-09 01:16:05 -0400

    use speed mode for dexpreopt

--- target/product/go_defaults_common.mk
+++ target/product/go_defaults_common.mk
@@ -21,9 +21,6 @@
 PRODUCT_VENDOR_PROPERTIES += \
      ro.config.low_ram=true \
 
-# Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
 # Always preopt extracted APKs to prevent extracting out of the APK for gms
 # modules.
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
